0f80231dd732afb92808fdd58aa0d353247e74e2
Nikhil Vikraman <nikhil.v@neoito.com>
2023-01-13 11:52:29 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Todotest1 from './todoTest1'
+export default function App() {
+  return (
+    <div>
+      <Todotest1/>
+    </div>
+  )
+}
---
+++ src/todoTest1.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const Todotest1 = () => {
+  return (
+   <p>Hello from todoTest1 </p>
+  )
+}
+export default Todotest1
